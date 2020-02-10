491571b8719aee9dbd9e2161f850a4ae18d0e1cf
Chris <mr.skytte@me.com>
2020-02-10 10:10:20 +0100

    first use exercise on string using back ticks

--- basics.js
+++ basics.js
@@ -1,1 +1,10 @@
-console.log(`hello world`);
+"use strict";
+
+let name = "Chris";
+let pet = "dog";
+let petName = "Piper";
+
+function consoleLog() {
+  console.log(`My name is ${name}.
+I have a ${pet} called ${petName}`);
+}
