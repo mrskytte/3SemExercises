fae1443a843888a5b33f0a6e55cbecc42e949ffb
Chris <mr.skytte@me.com>
2020-02-10 11:38:39 +0100

    Capitilize first letter

--- capitalization3.js
+++ capitalization3.js
@@ -5,6 +5,4 @@ let name = "Joanathan";
 console.log(name.toLowerCase());
 name = name.toLowerCase();
 
-console.log(
-  name.substring(0, 2) + name[2].toUpperCase() + name.substring(3, name.length)
-);
+console.log(name[0].toUpperCase() + name.substring(1, name.length));
