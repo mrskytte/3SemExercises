9ea4e994e3092a8e707da96c0ba85252ab5e72ef
Chris <mr.skytte@me.com>
2020-03-08 21:16:50 +0100

    added different cursors and structured code better

--- mouseevents.js
+++ mouseevents.js
@@ -11,34 +11,49 @@ svg.addEventListener("mousedown", activateMove);
 
 function activateMove() {
   event.preventDefault();
-  svg.addEventListener("mousemove", movingMouse);
-  window.addEventListener("mouseup", () =>
-    svg.removeEventListener("mousemove", movingMouse)
-  );
-  function movingMouse() {
-    clipCircle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
-    clipCircle.setAttribute(
-      "cy",
-      (event.pageY / window.innerHeight) *
-        869 *
-        (window.innerHeight / window.innerWidth)
-    );
-    circle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
-    circle.setAttribute(
-      "cy",
-      ((event.pageY / window.innerHeight) * 869 * window.innerHeight) /
-        window.innerWidth
-    );
+  movingMouse();
+  showCircles();
+  document.querySelector("svg").style.setProperty("--cursor", "crosshair");
 
-    clippedImg.setAttribute(
-      "x",
-      ((event.pageX * -1) / window.innerWidth) * 869
-    );
-    console.log(event.pageX);
-    clippedImg.setAttribute(
-      "y",
-      (((event.pageY * -1) / window.innerHeight) * 869 * window.innerHeight) /
-        window.innerWidth
-    );
-  }
+  svg.addEventListener("mousemove", movingMouse);
+  window.addEventListener("mouseup", mouseUp);
+}
+
+function movingMouse() {
+  clipCircle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
+  clipCircle.setAttribute(
+    "cy",
+    (event.pageY / window.innerHeight) *
+      869 *
+      (window.innerHeight / window.innerWidth)
+  );
+  circle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
+  circle.setAttribute(
+    "cy",
+    ((event.pageY / window.innerHeight) * 869 * window.innerHeight) /
+      window.innerWidth
+  );
+  clippedImg.setAttribute("x", ((event.pageX * -1) / window.innerWidth) * 869);
+  console.log(event.pageX);
+  clippedImg.setAttribute(
+    "y",
+    (((event.pageY * -1) / window.innerHeight) * 869 * window.innerHeight) /
+      window.innerWidth
+  );
+}
+
+function mouseUp() {
+  svg.removeEventListener("mousemove", movingMouse);
+  document.querySelector("svg").style.setProperty("--cursor", "zoom-in");
+  hideCircles();
+}
+
+function showCircles() {
+  document.querySelector("#circle").classList.remove("hide");
+  document.querySelector("#clipcircle").classList.remove("hide");
+}
+
+function hideCircles() {
+  document.querySelector("#circle").classList.add("hide");
+  document.querySelector("#clipcircle").classList.add("hide");
 }
