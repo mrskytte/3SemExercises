92b837982e7fac8051ac2f46c49de03dba42d979
Chris <mr.skytte@me.com>
2020-03-08 20:55:50 +0100

    stuff added

--- mouseevents.js
+++ mouseevents.js
@@ -16,22 +16,29 @@ function activateMove() {
     svg.removeEventListener("mousemove", movingMouse)
   );
   function movingMouse() {
-    clipCircle.setAttribute("cx", (event.pageX / window.innerWidth) * 1000);
+    clipCircle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
     clipCircle.setAttribute(
       "cy",
       (event.pageY / window.innerHeight) *
-        1000 *
+        869 *
         (window.innerHeight / window.innerWidth)
     );
-    circle.setAttribute("cx", (event.pageX / window.innerWidth) * 1000);
+    circle.setAttribute("cx", (event.pageX / window.innerWidth) * 869);
     circle.setAttribute(
       "cy",
-      (event.pageY / window.innerHeight) *
-        1000 *
-        (window.innerHeight / window.innerWidth)
+      ((event.pageY / window.innerHeight) * 869 * window.innerHeight) /
+        window.innerWidth
     );
-    clippedImg.setAttribute("x", 400 - event.pageX * 0.5);
-    console.log(clippedImg.attributes);
-    clippedImg.setAttribute("y", 200 - event.pageY * 0.5);
+
+    clippedImg.setAttribute(
+      "x",
+      ((event.pageX * -1) / window.innerWidth) * 869
+    );
+    console.log(event.pageX);
+    clippedImg.setAttribute(
+      "y",
+      (((event.pageY * -1) / window.innerHeight) * 869 * window.innerHeight) /
+        window.innerWidth
+    );
   }
 }
